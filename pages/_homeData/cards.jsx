99a27250ba6b9741ad8e34579eec321a9ec27d38
carlos Feliz <cfelizmedina@gmail.com>
2022-04-21 17:17:18 -0400

    Update cards and items components

---
+++ pages/_homeData/cards.jsx
@@ -0,0 +1,62 @@
+/* eslint-disable react/no-unescaped-entities */
+/* eslint-disable @next/next/no-img-element */
+function Cards() {
+  return (
+    <div className="projectContainer flexRow">
+      <div className="card cardShadow" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://cdn3.iconfinder.com/data/icons/online-states/150/Photos-1024.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <div className="card cardShadow" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://cdn3.iconfinder.com/data/icons/online-states/150/Photos-1024.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <div className="card cardShadow" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://cdn3.iconfinder.com/data/icons/online-states/150/Photos-1024.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+      <div className="card cardShadow" style={{ width: "18rem" }}>
+        <img
+          className="card-img-top"
+          src="https://cdn3.iconfinder.com/data/icons/online-states/150/Photos-1024.png"
+          alt="Card image cap"
+        />
+        <div className="card-body">
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
