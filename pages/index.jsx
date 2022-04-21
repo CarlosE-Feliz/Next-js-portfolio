29107c7a4612c059f07568d5e1c9c06468f56509
carlos Feliz <cfelizmedina@gmail.com>
2022-04-21 17:22:47 -0400

    NetxJs portfolio work in process

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,23 +1,27 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable @next/next/no-img-element */
+import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import Cards from "./_homeData/cards";
+import Items from "./_homeData/items";
 import NavBar from "./_navBar";
 
 function Home() {
   const techContainer = React.createRef();
-  const [controlScroll, setControlScroll] = useState(false);
+  const router = useRouter();
+  // const [controlScroll, setControlScroll] = useState(false);
 
-  if (typeof window !== "undefined") {
-    window.addEventListener("scroll", function () {
-      if (techContainer.current) {
-        let positionTech = techContainer.current.getBoundingClientRect().top;
-        let windowHeight = window.innerHeight / 1.5;
-        if (positionTech < windowHeight) {
-          setControlScroll(() => true);
-        }
-      }
-    });
-  }
+  // if (typeof window !== "undefined") {
+  //   window.addEventListener("scroll", function () {
+  //     if (techContainer.current) {
+  //       let positionTech = techContainer.current.getBoundingClientRect().top;
+  //       let windowHeight = window.innerHeight / 1.5;
+  //       if (positionTech < windowHeight) {
+  //         setControlScroll(() => true);
+  //       }
+  //     }
+  //   });
+  // }
 
   return (
     <>
@@ -32,86 +36,8 @@ function Home() {
           </p>
         </div>
         <div className="info flexColumn">
-          <div className="projectContainer flexRow">
-            <div className="card cardShadow" style={{ width: "18rem" }}>
-              <img className="card-img-top" src="#" alt="Card image cap" />
-              <div className="card-body">
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-            <div className="card cardShadow" style={{ width: "18rem" }}>
-              <img className="card-img-top" src="#" alt="Card image cap" />
-              <div className="card-body">
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-            <div className="card cardShadow" style={{ width: "18rem" }}>
-              <img className="card-img-top" src="#" alt="Card image cap" />
-              <div className="card-body">
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-            <div className="card cardShadow" style={{ width: "18rem" }}>
-              <img className="card-img-top" src="#" alt="Card image cap" />
-              <div className="card-body">
-                <p className="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div>
-            <div className="thecContainer flexRow">
-              <div className="item item1 relative">
-                <p>JavaScript</p>
-              </div>
-              <div className="item item2 relative">
-                <p>React</p>
-              </div>
-              <div className="item item3 relative">
-                <p>Redux</p>
-              </div>
-              <div className="item item3 relative">
-                <p>CSS/SCSS</p>
-              </div>
-              <div className="item item2 relative">
-                <p>Next.js</p>
-              </div>
-              <div className="item item1 relative">
-                <p>TypeScript</p>
-              </div>
-            </div>
-            <div className="thecContainer flexRow">
-              <div className="item itemT1 relative">
-                <p>JavaScript</p>
-              </div>
-              <div className="item itemT2 relative">
-                <p>React</p>
-              </div>
-              <div className="item itemT3 relative">
-                <p>Redux</p>
-              </div>
-              <div className="item itemT3 relative">
-                <p>CSS/SCSS</p>
-              </div>
-              <div className="item itemT2 relative">
-                <p>Next.js</p>
-              </div>
-              <div className="item itemT1 relative">
-                <p>TypeScript</p>
-              </div>
-            </div>
-          </div>
+          <Cards />
+          <Items />
         </div>
         <div className="footer flexRow">
           <div className="footerItem1">
